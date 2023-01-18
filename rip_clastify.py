bc96495b2daec41da698d501bb0c399708ba18c8
fncraft <86068980+fncraft@users.noreply.github.com>
2023-01-18 20:39:54 +0000

    back to 5 lines😓

--- rip_clastify.py
+++ rip_clastify.py
@@ -1,4 +1,5 @@
 from mitmproxy import http
 def response(flow: http.HTTPFlow) -> None:
     if flow.request.path.endswith("can-view"):
+        flow.response.status_code = 200
         flow.response.text = "ok"
