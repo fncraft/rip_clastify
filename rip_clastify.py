32ab902bd40aadce3da7bd1b92711fa129b5315a
fncraft <86068980+fncraft@users.noreply.github.com>
2023-01-18 20:32:17 +0000

    i forgot to remove this import

--- rip_clastify.py
+++ rip_clastify.py
@@ -1,5 +1,4 @@
 from mitmproxy import http
-from mitmproxy import ctx
 def response(flow: http.HTTPFlow) -> None:
     if flow.request.path.endswith("can-view"):
         flow.response.status_code = 200
